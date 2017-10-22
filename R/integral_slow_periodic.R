822f6ea2ebd6e4fa5bb6557bea5a198bfabbf7e0
sjnj <sjnjare@gmail.com>
2017-10-23 01:11:15 +0200

    version0.1
    examples and code on the same .R

--- R/integral_slow_periodic.R
+++ R/integral_slow_periodic.R
@@ -95,5 +95,24 @@ Int.int_slow_periodic <- function(obj,ti,tf){
 
 
 
+###Examples#################
+
+
+#First example:
+
+I1<-int_slow_periodic(function(t) 1/t,c(1),c(0,1,0),100,2)
+
+integrate(function(t){1/t*(cos(100*t))},1,2)
+
+Int.int_slow_periodic(I1,1,2)
+
+#Second example
+
+I2<- int_slow_periodic(function(t) t^2,c(1,2),c(1,0.5,0,0,1),100,2)
+
+integrate(function(t)t^2*(1+0.5*cos(100*t)+sin(2*100*t)),1,2)
+
+#As expected the error is around 1%
+(Int.int_slow_periodic(I2,1,2)-integrate(function(t)t^2*(1+0.5*cos(100*t)+sin(2*100*t)),1,2)$value)/integrate(function(t)t^2*(1+0.5*cos(100*t)+sin(2*100*t)),1,2)$value
 
 
